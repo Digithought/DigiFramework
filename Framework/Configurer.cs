c7106f6e467966df5ad403c6f8d5b30da5ceae3e
n8allan <nate@databasecg.com>
2015-10-01 00:03:59 -0600

    Added very simple configuration and data logging utilities.  Updated Json.Net version.

---
+++ Framework/Configurer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Digithought.Framework
+{
+    public static class Configurer
+	{
+		public static T Get<T>(T defaultConfig)
+		{
+			string config = null;
+			var fileName = System.Reflection.Assembly.GetEntryAssembly().Location + "." + typeof(T).Name + ".config";
+			try
+			{
+				if (File.Exists(fileName))
+				{
+					config = File.ReadAllText(fileName);
+					var copy = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(Newtonsoft.Json.JsonConvert.SerializeObject(defaultConfig));
+					Newtonsoft.Json.JsonConvert.PopulateObject(config, copy);
+					return copy;
+				}
+				else
+				{
+					config = Newtonsoft.Json.JsonConvert.SerializeObject(defaultConfig);
+					File.WriteAllText(fileName, config);
+				}
+			}
+			catch (Exception e)
+			{
+				Logging.Error(e);
+				// Don't rethrow - this isn't critical
+			}
+			finally
+			{
+				#if (TRACE_CONFIGURATION)
+				Logging.Trace("Configuration", typeof(T).Name + ": \t" + config);
+				#endif
+			}
+			return defaultConfig;
+		}
+
+	}
+}
---
+++ Framework/Data.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Digithought.Framework
+{
+    public static class Data
+	{
+		public static event LogDataHandler LogData;
+		public static LogDataFilterHandler LogFilter;
+		public static System.Text.RegularExpressions.Regex TopicMask = null;
+
+		[System.Diagnostics.Conditional("FRAMEWORK_DATALOGGING")]
+		public static void Log(DataHeader header, params object[] values)
+		{
+			LogTo(header, null, values);
+		}
+
+		public static void LogTo(DataHeader header, string fileName, params object[] values)
+		{
+			try
+			{
+				var filter = LogFilter;	// Capture
+				var mask = TopicMask;	// Capture
+				if
+					(
+						(filter == null || filter(header, values))
+							&& (mask == null || mask.IsMatch(header.Topic))
+					)
+				{
+					var handler = LogData;	// Capture
+					if (handler != null)
+						handler(header, fileName, values);
+				}
+			}
+			catch (Exception e)
+			{
+				System.Diagnostics.Debug.WriteLine("Error attempting to log data: \r\n" + e.ToString());
+				// Don't rethrow
+			}
+		}
+	}
+
+	public delegate void LogDataHandler(DataHeader header, string fileName, object[] values);
+
+	public delegate bool LogDataFilterHandler(DataHeader header, object[] values);
+
+	public class DataHeader
+	{
+		public DataHeader(string topic, params string[] valueDescriptions)
+		{
+			Topic = topic;
+			ValueDescriptions = valueDescriptions;
+		}
+
+		public object Tag { get; set; }
+
+		public string Topic { get; private set; }
+
+		public string[] ValueDescriptions { get; private set; }
+	}
+}
