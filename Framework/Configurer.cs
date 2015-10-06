5fa87037ad002dffcb48d95a5805475db89dae3f
n8allan <nate@databasecg.com>
2015-10-06 12:33:26 -0600

    Added object cloning utility.  Made configurer reuse child objects and re-write configuration on error.  Added inherited attribute propogation to configurer.

--- Framework/Configurer.cs
+++ Framework/Configurer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
+using System.Reflection;
 
 namespace Digithought.Framework
 {
@@ -7,36 +9,113 @@ namespace Digithought.Framework
 	{
 		public static T Get<T>(T defaultConfig)
 		{
+			return Get(defaultConfig, typeof(T).Name);
+		}
+
+		public static T Get<T>(T defaultConfig, string name)
+		{
 			string config = null;
-			var fileName = System.Reflection.Assembly.GetEntryAssembly().Location + "." + typeof(T).Name + ".config";
+			var fileName = System.Reflection.Assembly.GetEntryAssembly().Location + "." + name + ".config";
 			try
 			{
 				if (File.Exists(fileName))
 				{
 					config = File.ReadAllText(fileName);
-					var copy = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(Newtonsoft.Json.JsonConvert.SerializeObject(defaultConfig));
-					Newtonsoft.Json.JsonConvert.PopulateObject(config, copy);
+					var copy = defaultConfig.Clone();
+					var errorLoading = false;
+					var jsonSettings = new Newtonsoft.Json.JsonSerializerSettings 
+						{ 
+							Error = (o, e) => { errorLoading = true; }, 
+							ObjectCreationHandling = Newtonsoft.Json.ObjectCreationHandling.Reuse 
+						};
+                    Newtonsoft.Json.JsonConvert.PopulateObject(config, copy, jsonSettings);
+					if (errorLoading)
+						try
+						{
+							SaveObject(copy, fileName);
+						}
+						catch (Exception e)
+						{
+							Logging.Error(e);
+							// Don't rethrow - it's okay if we can't rewrite the config file
+						}
 					return copy;
 				}
 				else
 				{
-					config = Newtonsoft.Json.JsonConvert.SerializeObject(defaultConfig);
-					File.WriteAllText(fileName, config);
+					SaveObject(defaultConfig, fileName);
 				}
 			}
 			catch (Exception e)
 			{
 				Logging.Error(e);
-				// Don't rethrow - this isn't critical
+				// Don't rethrow - we'll just use the default
 			}
 			finally
 			{
 				#if (TRACE_CONFIGURATION)
-				Logging.Trace("Configuration", typeof(T).Name + ": \t" + config);
+				Logging.Trace("Configuration", name + ": \t" + config);
 				#endif
 			}
 			return defaultConfig;
 		}
 
+		/// <summary> Copies values from the root of the configuration object to any sub-configuration objects which have the same property names but no value. </summary>
+		public static void ApplyInherited(object config)
+		{
+			foreach (var member in 
+				config.GetType().FindMembers
+				(
+					MemberTypes.Field | MemberTypes.Property, BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy, 
+					(m, c) => !(m is PropertyInfo) || ((PropertyInfo)m).GetIndexParameters().Length == 0, 
+					null
+				)
+			)
+			{
+				InternalApply(member, config, member is FieldInfo ? ((FieldInfo)member).GetValue(config) : ((PropertyInfo)member).GetValue(config));
+			}
+		}
+
+		private static void InternalApply(MemberInfo toApply, object config, object applyValue)
+		{
+			if (config == null || config.GetType().IsValueType)
+				return;
+			if (config is IEnumerable)
+			{
+				foreach (var item in (IEnumerable)config)
+					InternalApply(toApply, item, applyValue);
+			}
+			else
+			{
+				foreach (var member in
+					config.GetType().FindMembers
+					(
+						MemberTypes.Field | MemberTypes.Property, BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy,
+						(m, c) => !(m is PropertyInfo) || ((PropertyInfo)m).GetIndexParameters().Length == 0,
+						null
+					)
+				)
+				{
+					var value = member is FieldInfo ? ((FieldInfo)member).GetValue(config) : ((PropertyInfo)member).GetValue(config);
+					if (toApply != member && member.Name == toApply.Name && member.MemberType == toApply.MemberType)
+					{
+						if (value == null)
+						{
+							if (member is FieldInfo)
+								((FieldInfo)member).SetValue(config, applyValue);
+							else
+								((PropertyInfo)member).SetValue(config, applyValue);
+						}
+					}
+					else
+						InternalApply(toApply, value, applyValue);
+				}
+			}
+		}
+
+		private static void SaveObject<T>(T instance, string fileName)
+		{
+			File.WriteAllText(fileName, Newtonsoft.Json.JsonConvert.SerializeObject(instance));
+		}
 	}
 }
---
+++ Framework/ObjectUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digithought.Framework
+{
+	public static class ObjectUtility
+	{
+		/// <summary> Makes a deep copy of an object using a serializer/deserializer. </summary>
+		public static T Clone<T>(this T value)
+		{
+			return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(Newtonsoft.Json.JsonConvert.SerializeObject(value));
+		}
+
+		/// <summary> Makes a deep copy of an object and provides an opportunity to perform additional initialization on the result. </summary>
+		public static T Clone<T>(this T value, Action<T> initializer)
+		{
+			var result = value.Clone();
+			if (initializer != null)
+				initializer(result);
+			return result;
+		}
+	}
+}
