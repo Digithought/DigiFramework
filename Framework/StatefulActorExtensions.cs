40ad92fbcd1cdbbec81089e04f8e30f79fc1a134
Nathan Allan <nate@digithought.com>
2021-03-12 10:33:45 -0700

    Added WaitForState extension method

---
+++ Framework/StatefulActorExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Digithought.Framework
+{
+	public static class StatefulActorExtensions
+	{
+		public static void WaitForState<TActor, TState, TTrigger>(this IStatefulActor<TActor, TState, TTrigger> actor, TState desired, int timeout)
+			where TState : struct
+		{
+			var completionSource = new System.Threading.Tasks.TaskCompletionSource<bool>();
+			void waitForUnstarted(TState oldState, StateMachine<TState, TTrigger>.Transition transition)
+			{
+				if (actor.InState(desired))
+					completionSource.TrySetResult(true);
+			};
+			actor.StateChanged += waitForUnstarted;
+
+			// Check for synchronous completion
+			if (actor.InState(desired))
+				completionSource.TrySetResult(true);
+
+			completionSource.Task.Wait(timeout);
+			actor.StateChanged -= waitForUnstarted;
+		}
+	}
+}
